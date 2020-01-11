a13c1bc2aaeadfdcda4fbaf6288cd82db430a0e0
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-01-11 15:16:14 +0300

    Update define.php

--- define.php
+++ define.php
@@ -3,12 +3,7 @@ $file = fopen("data.txt", "w+");
 fwrite($file, "");
 fclose($file);
 
-echo "DD      FFFFFF";
-echo "D  D    F";
-echo "D   D   FFFFFF";
-echo "D   D   F";
-echo "D  D    F";
-echo "DD      F";
+echo "DEFINE";
 echo "https://vk.com/termuxlab | termux - Lab";
 system("php -S 127.0.0.1:666");
 ?>
